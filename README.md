313eb30c74f98c4240318b29f9ba7b5a774ee86a
Christian Lopez <calv.catfor@gmail.com>
2022-08-24 14:22:02 -0500

    Added Auth JWT authentication

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # GCP_Flask_Firebase
 
-This is a simple API CRUD in FLASK + Firebase Demo
+This is a simple API CRUD in FLASK + Firebase Demo protecting the APIs with JWT
 
 ## The highlights are:
     - It was a quickly developed project/setup for showing base skills for developing
-    - Currently only http errors such as 404 & 500 are handled with custom pages renders
-    - This repo is connected with a Trigger in Google Cloud Build for automatic deploy after pushing changes 
+    - Currently only http errors such as 404 & 500 are handled with custom pages renders for this server
+    - This repo is connected with a Trigger in Google Cloud Build for automatic deploy after pushing changes  and deployed in Google Run
     - Has the simple yet useful added logging configuration ofr showing logs in Google Cloud Run/Log Explorer
+    - Google secrets are being used + Env Vars for keeping safe some secrets for connecting with Firebase
+    - The interface is in another project using VUE
 
 ## Downside
-    - Currently this lacks of any proper UI for managing the CRUD for a common final user, it's mostly likely for a developer with min experience with Restful APIs
+    - Currently this lacks of any proper UI for managing the CRUD for a common final user here, it's mostly likely for a developer with min experience with Restful APIs
 
 ### TODO
-    - Add switch the frontend interface using VUE.js + Buefy UI and add controls for final users to use the api in the UI
     - Add example code for using Sendgrid by Twilio for validating emails validating format and validating 'A' and 'MX' DNS records
-    - Add proper and documented Google Secrets usage for avoid exposing credentials
     - Add unittesting with pytest into the yaml file
     - Add Telegram realtime notifications
-    - Add examples for APIs usage / flask-apispec
-    - and ...
+    - Add examples for APIs usage / flask-apispec other than the site-map
+    - Add SSO/OAuth capabilities [WIP]
+    - and other stuff
 
 ## Get hired
 
-Last Update 2022 Jul 22 
+Last Update 2022 Aug 24
--- app.py
+++ app.py
@@ -1,17 +1,39 @@
 # app.py
 
 # Required imports
-import os, logging, uuid
+import os, logging, uuid, pathlib, requests, hmac, hashlib
+from collections.abc import Mapping
+from functools import wraps
+# import google.cloud.logging
 
-import google.cloud.logging
-
-from flask import Flask, request, jsonify, render_template, Blueprint, url_for
+from flask import Flask, request, jsonify, render_template, Blueprint, url_for, session, abort, redirect
+from flask_jwt import JWT, jwt_required, current_identity
 from flask_cors import CORS
 from firebase_admin import credentials, firestore, initialize_app
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
+
+app = Flask("GCP_FLSK_FRBS")  #naming our application
+app.secret_key = "christianlopez.mx"  #it is necessary to set a password when dealing with OAuth 2.0
+
+#   Params necesary for working with the google OAuth
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"  #this is to set our environment to https because OAuth 2.0 only supports https environments
+GOOGLE_CLIENT_ID = "410342830455-ellv6s7gi13mhcd1cgpuaqk1qnpjts7l.apps.googleusercontent.com"  #enter your client id you got from Google console
+# GOOGLE_CLIENT_ID = "362896790228-enbe8d57gn1s1npbl29vikd8dppmhiti.apps.googleusercontent.com"  #enter your client id you got from Google console
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")  #set the path to where the .json file you got Google console is
+
+#   This is for the Google OAuth for login in with the googleaccount
+flow = Flow.from_client_secrets_file(  #Flow is OAuth 2.0 a class that stores all the information on how we want to authorize our users
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],  #here we are specifing what do we get after the authorization
+    redirect_uri="http://127.0.0.1:8080/callback"  #and the redirect URI is the point where the user will end up after the authorization
+)
 
 #init logger
-client = google.cloud.logging.Client()
-client.setup_logging()
+# client = google.cloud.logging.Client()
+# client.setup_logging()
 
 # # Commented logging format as in gcloud logging is ignored
 # LOGGING_FORMAT = "[%(filename)s:%(lineno)d] %(message)s"
@@ -20,7 +42,7 @@ lg = logging.getLogger(__name__)
 
 
 # Initialize Flask app
-app = Flask(__name__)
+# app = Flask(__name__)
 
 #The blueprint for raw APIs in flask
 bp = Blueprint('apisv1', __name__, url_prefix='/api/v1')
@@ -28,7 +50,7 @@ bp = Blueprint('apisv1', __name__, url_prefix='/api/v1')
 demo = Blueprint('demov1', __name__, url_prefix='/api/v1/vue')
 
 # Look forward the file in a secret related in Google Run
-cred = credentials.Certificate(os.getenv("firebase"))
+cred = credentials.Certificate("./key.json")
 default_app = initialize_app(cred)
 db = firestore.client()
 
@@ -36,8 +58,28 @@ db = firestore.client()
 fire_db = db.collection('demo') 
 
 # For implementing a UI w/ CRUD (in VUE.js)
+user_db = db.collection('users') 
 contact_db = db.collection('contacts') 
 department_db = db.collection('departments')
+
+# For logging sing-ups / logins
+oauth2_db = db.collection('log_oauth2') 
+
+class simple_user:
+    id=0
+    def __init__(self, id, username="dummy"):
+        self.id = id
+        self.username = username
+
+def authenticate(username, password):
+    lg.info(f"Authenticating {username}")
+    user = user_db.where('username', '==', username).get()
+    if user:
+        user = user[0].to_dict()
+        if hmac.compare_digest(user["password"].encode('utf-8'), hashlib.md5(password.encode('utf-8')).hexdigest().encode('utf-8')):
+            return simple_user(str(uuid.uuid4())[:8],user["username"])
+
+jwt = JWT(app, authenticate)
 
 
 def has_no_empty_params(rule):
@@ -45,8 +87,8 @@ def has_no_empty_params(rule):
     arguments = rule.arguments if rule.arguments is not None else ()
     return len(defaults) >= len(arguments)
 
-
 @app.route("/site-map")
+@jwt_required()
 def site_map():
     links = []
     for rule in app.url_map.iter_rules():
@@ -73,6 +115,7 @@ def error_raise():
 
 # Api for testing/debugging response codes
 @bp.route('/testcode/<code>', methods=['GET'])
+@jwt_required()
 def debug_response(code=200):
     lg.info("running debug_response")
     if code == 404:
@@ -82,6 +125,7 @@ def debug_response(code=200):
     return "", code
 
 @bp.route('/', methods=['GET'])
+@jwt_required()
 def im_root():
     lg.info("running im_root")
     """
@@ -94,6 +138,7 @@ def im_root():
 
 
 @bp.route('/add', methods=['POST'])
+@jwt_required()
 def create():
     lg.info("running create")
     """
@@ -119,6 +164,7 @@ def create():
 
 @bp.route('/list', methods=['GET'])
 @bp.route('/list/<id>', methods=['GET'])
+@jwt_required()
 def read(id=None):
     lg.info("running read")
     """
@@ -139,6 +185,7 @@ def read(id=None):
         return f"An Error Occurred: {e}", 500
 
 @bp.route('/update', methods=['POST', 'PUT'])
+@jwt_required()
 def update():
     lg.info("running update")
     """
@@ -155,6 +202,7 @@ def update():
 
 @bp.route('/delete', methods=['GET', 'DELETE'])
 @bp.route('/delete/<id>', methods=['GET', 'DELETE'])
+@jwt_required()
 def delete(id=None):
     lg.info("running delete")
     """
@@ -172,6 +220,7 @@ def delete(id=None):
 # these are for the CRUD in VUE.js
 
 @demo.route('contacts/add', methods=['POST'])
+@jwt_required()
 def contact_create():
     lg.info("running contact create")
     try:
@@ -191,6 +240,7 @@ def contact_create():
 
 @demo.route('contacts', methods=['GET'])
 @demo.route('contacts/<id>', methods=['GET'])
+@jwt_required()
 def contact_read(id=None):
     lg.info("running contact read")
     try:
@@ -206,6 +256,7 @@ def contact_read(id=None):
         return f"An Error Occurred: {e}", 500
 
 @demo.route('contacts/update', methods=['POST', 'PUT'])
+@jwt_required()
 def contact_update():
     lg.info("running contact update")
     try:
@@ -218,6 +269,7 @@ def contact_update():
 
 @demo.route('contacts/delete', methods=['GET', 'DELETE'])
 @demo.route('contacts/delete/<id>', methods=['GET', 'DELETE'])
+@jwt_required()
 def contact_delete(id=None):
     lg.info("running contact delete")
     try:
@@ -232,6 +284,7 @@ def contact_delete(id=None):
 #   For creating the departments
 
 @demo.route('departments/add', methods=['POST'])
+@jwt_required()
 def department_create():
     lg.info("running department create")
     try:
@@ -249,6 +302,7 @@ def department_create():
 
 @demo.route('departments', methods=['GET'])
 @demo.route('departments/<id>', methods=['GET'])
+@jwt_required()
 def department_read(id=None):
     lg.info("running department read")
     try:
@@ -264,6 +318,7 @@ def department_read(id=None):
         return f"An Error Occurred: {e}", 500
 
 @demo.route('departments/update', methods=['POST', 'PUT'])
+@jwt_required()
 def department_update():
     lg.info("running department update")
     try:
@@ -275,6 +330,7 @@ def department_update():
 
 @demo.route('departments/delete', methods=['GET', 'DELETE'])
 @demo.route('departments/delete/<id>', methods=['GET', 'DELETE'])
+@jwt_required()
 def department_delete(id=None):
     lg.info("running department delete")
     try:
@@ -284,6 +340,81 @@ def department_delete(id=None):
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An Error Occurred: {e}", 500
+
+
+# For adding OAuth process [WIP]
+
+def login_is_required(function):  #a function to check if the user is authorized or not
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:  #authorization required
+            return abort(401)
+        else:
+            return function()
+
+    return wrapper
+
+
+@app.route("/login")  #the page where the user can login
+def login():
+    authorization_url, state = flow.authorization_url()  #asking the flow class for the authorization (login) url
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")  #this is the page that will handle the callback process meaning process after the authorization
+def callback():
+    # lg.info(request.json)
+    flow.fetch_token(authorization_response=request.url)
+    id = None
+    # if "id" not in request.json:
+    #     id = str(uuid.uuid4())[:8]
+    #     request.json['id'] = id
+    # else:
+    #     id = request.json['id']
+    # if oauth2_db.document(id).get().to_dict() is None:
+    #     oauth2_db.document(id).set(request.json)
+    # if not session["state"] == request.args["state"]:
+    #     abort(500)  #state does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    print(id_info)
+    print(type(id_info))
+    print(dir(id_info))
+    # oauth2_db
+    # id = str(uuid.uuid4())[:8]
+    # request.json['id'] = id
+    session["data"] = id_info
+    session["google_id"] = id_info.get("sub")  #defing the results to show on the page
+    session["name"] = id_info.get("name")
+    return redirect("/protected_area")  #the final page where the authorized users will end up
+
+
+@app.route("/logout")  #the logout page and function
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/")  #the home page where the login button will be located
+def index():
+    return "Hello World <a href='/login'><button>Login</button></a>"
+
+
+@app.route("/protected_area")  #the page where only the authorized users can go to
+@login_is_required
+def protected_area():
+    return f"Hello {session['data']}! <br/> <a href='/logout'><button>Logout</button></a>"  #the logout button 
+
 
 
 
